1edc2cb0907e2753669d0fcd941902a99cd42cf0
Eric <stephen.erich243@gmail.com>
2024-05-09 23:51:20 +0100

    corrected

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -9,47 +9,41 @@ can execute only two operations in this file: `Copy All` and `Paste`.
 """
 
 
-def is_prime(n):
-    """function to check if a number is prime or not"""
+def is_prime(n, primes):
+    """Function to calculate if a number is prime"""
     if n <= 1:
         return False
-
-    for i in range(2, int(n ** 0.5) + 1):
-        if n % i == 0:
+    if n == 2:
+        return True
+    if n % 2 == 0:
+        return False
+    for p in primes:
+        if p * p > n:
+            break
+        if n % p == 0:
             return False
     return True
 
 
-def generate_prime(n):
-    """function to generate prime numbers up to n"""
-    prime = []
-    for i in range(2, n+1):
-        if is_prime(i):
-            prime.append(i)
-    return prime
-
-
 def prime_fact(n):
-    """function to generate prime factors of a number"""
+    """Function to calculate the prime factors of a number"""
     prime_factor = []
-    prime = generate_prime(n)
-
-    for i in prime:
-        while n % i == 0:
-            prime_factor.append(i)
-            n //= i
+    primes = [2]
+    for i in range(3, int(n**0.5) + 1, 2):
+        if is_prime(i, primes):
+            primes.append(i)
+    for p in primes:
+        while n % p == 0:
+            prime_factor.append(p)
+            n //= p
+    if n > 1:
+        prime_factor.append(n)
     return prime_factor
 
 
 def minOperations(n):
-    """function to calculate the fewest number of operations
-    needed to result in exactly n H characters in the file"""
+    """Function to calculate the minimum operations to reach n characters"""
     if n <= 1:
         return 0
     number_operations = prime_fact(n)
-    sum = 0
-
-    for operation in number_operations:
-        sum += operation
-
-    return sum
+    return sum(number_operations)
