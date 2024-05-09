820e288d2a793944c144eda47957e1c0622ac888
Eric <stephen.erich243@gmail.com>
2024-05-09 23:38:59 +0100

    corrected

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -6,20 +6,47 @@ needed to result in exactly n H characters in the file.
 """
 
 
+def is_prime(n):
+    """function to check if a number is prime or not"""
+    if n <= 1:
+        return False
+
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+
+def generate_prime(n):
+    """function to generate prime numbers up to n"""
+    prime = []
+    for i in range(2, n+1):
+        if is_prime(i):
+            prime.append(i)
+    return prime
+
+
+def prime_fact(n):
+    """function to generate prime factors of a number"""
+    prime_factor = []
+    prime = generate_prime(n)
+
+    for i in prime:
+        while n % i == 0:
+            prime_factor.append(i)
+            n //= i
+    return prime_factor
+
+
 def minOperations(n):
-    """
-    Calculates the fewest number of operations
-    needed to result in exactly n H characters in the file.
-    Args:
-        n (int): length of letter `H` required in the file
-    Returns:
-        (int): number of minimum operations if possible otherwise 0
-    """
-    minimumOperations = 2
-    totalOperations = 0
-    while n > 1:
-        while n % minimumOperations == 0:
-            totalOperations += minimumOperations
-            n /= minimumOperations
-        minimumOperations += 1
-    return totalOperations
+    """function to calculate the fewest number of operations
+    needed to result in exactly n H characters in the file"""
+    if n <= 1:
+        return 0
+    number_operations = prime_fact(n)
+    sum = 0
+
+    for operation in number_operations:
+        sum += operation
+
+    return sum
