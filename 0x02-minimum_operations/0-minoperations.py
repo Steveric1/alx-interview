a95ccfc37df59ab6ffc5226e29b1eb87278c37b7
Eric <stephen.erich243@gmail.com>
2024-05-09 23:40:58 +0100

    corrected

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,8 +1,11 @@
-#!/usr/bin/env python3
-
+#!/usr/bin/python3
 """
-Minimum Operations: method that calculates the fewest number of operations
-needed to result in exactly n H characters in the file.
+In a text file, there is a single character H. Your text editor
+can execute only two operations in this file: `Copy All` and `Paste`.
+
+@TODO:
+    Deines a method that calculates the fewest number of operations
+    needed to result in exactly n H characters in the file.
 """
 
 
