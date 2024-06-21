edd27fb3631791dc3de1151b75c42b8b47499668
Eric <stephen.erich243@gmail.com>
2024-06-21 02:29:55 +0000

    make change algorithm

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -14,16 +14,25 @@ def makeChange(coins: List[int], total: int) -> int:
         - total(int): total amount given
     Returns:
         return fewest number of coins needed to meet total
+    Algorithm Used:
+        Greedy algorithm
     """
 
     if total <= 0:
         return 0
 
-    dp = [float('inf')] * (total + 1)
-    dp[0] = 0
+    amount = total
+    coins_count = 0
+    coin_indx = 0
+    coins_len = len(coins)
+    sorted_coins = sorted(coins, reverse=True)
 
-    for coin in coins:
-        for amount in range(coin, total + 1):
-            dp[amount] = min(dp[amount], dp[amount - coin] + 1)
-
-    return dp[total] if dp[total] != float('inf') else -1
+    while amount > 0:
+        if coin_indx >= coins_len:
+            return -1
+        if amount - sorted_coins[coin_indx] >= 0:
+            amount -= sorted_coins[coin_indx]
+            coins_count += 1
+        else:
+            coin_indx += 1
+    return coins_count
